218174f3036738ed4ee669452e76cf4d20e8159f
kingsly <kingsly@kingslys-mbp>
2019-08-01 22:31:31 -0400

    mongodb connection error fixed

--- src/main/java/com/ewolff/microservice/catalog/CatalogApp.java
+++ src/main/java/com/ewolff/microservice/catalog/CatalogApp.java
@@ -5,11 +5,12 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 @ComponentScan
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @Component
 public class CatalogApp {
 
