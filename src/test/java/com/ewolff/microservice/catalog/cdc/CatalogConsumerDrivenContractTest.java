e257591eb08baae1bc71b37b38fda4c4a0127304
chakrav <avik_chakraborty@keybank.com>
2019-08-21 00:37:28 -0400

    test case fix

--- src/test/java/com/ewolff/microservice/catalog/cdc/CatalogConsumerDrivenContractTest.java
+++ src/test/java/com/ewolff/microservice/catalog/cdc/CatalogConsumerDrivenContractTest.java
@@ -1,40 +1,57 @@
-//package com.ewolff.microservice.catalog.cdc;
-//
-//import static org.junit.Assert.*;
-//
-//import java.util.Collection;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-//import org.springframework.test.context.ActiveProfiles;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//
-//import com.ewolff.microservice.catalog.CatalogApp;
-//
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@SpringBootTest(classes = CatalogApp.class, webEnvironment = WebEnvironment.DEFINED_PORT)
-//@ActiveProfiles("test")
-//public class CatalogConsumerDrivenContractTest {
-//
-//	@Autowired
-//	private CatalogClient catalogClient;
-//
-//	@Test
-//	public void testFindAll() {
-//		Collection<Item> result = catalogClient.findAll();
-//		assertEquals(1, result.stream()
-//				.filter(i -> (i.getName().equals("iPod") && i.getPrice() == 42.0 && i.getItemId() == 1)).count());
-//	}
-//
-//	@Test
-//	public void testGetOne() {
-//		Collection<Item> allItems = catalogClient.findAll();
-//		Long id = allItems.iterator().next().getItemId();
-//		Item result = catalogClient.getOne(id);
-//		assertEquals(id.longValue(), result.getItemId());
-//	}
-//
-//}
+package com.ewolff.microservice.catalog.cdc;
+
+import com.ewolff.microservice.catalog.CatalogApp;
+import com.ewolff.microservice.catalog.Item;
+import com.ewolff.microservice.catalog.ItemRepository;
+import com.ewolff.microservice.catalog.SpringRestDataConfig;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import static junit.framework.TestCase.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+@ContextConfiguration(classes = SpringRestDataConfig.class, loader = AnnotationConfigContextLoader.class)
+public class CatalogConsumerDrivenContractTest {
+
+	@InjectMocks
+	private CatalogApp customerApp;
+
+	@Mock
+	private ItemRepository itemRepository;
+
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+
+	}
+
+	@Test
+	public void testConstructor() {
+		CatalogApp catalogApp = new CatalogApp(itemRepository);
+
+		assertNotNull(catalogApp);
+	}
+
+	@Test
+	public void testGenerateTestData() {
+		CatalogApp custApp = new CatalogApp(itemRepository);
+		custApp.generateTestData();
+
+    	verify(itemRepository, times(14)).save(any(Item.class));
+	}
+
+
+
+}
